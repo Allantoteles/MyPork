fe4889e2b7456497fc9b6bff8896195f51b46108
Allantoteles <allan@example.com>
2025-12-27 23:06:07 -0300

    Update service worker and auth callback

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import { createClient } from '@/utils/supabase/server'
 
-export const runtime = 'edge'
+export const runtime = 'nodejs'
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
